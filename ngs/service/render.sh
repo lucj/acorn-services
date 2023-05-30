8861e4740486ca1e64ec47598eab3abd7cf1a903
Luc Juggery <luc.juggery@gmail.com>
2023-05-30 15:23:42 +0200

    additional details

--- ngs/service/render.sh
+++ ngs/service/render.sh
@@ -4,7 +4,7 @@
 echo $ACORN_EVENT
 
 if [ "$ACORN_EVENT" = "delete" ]; then
-  nsc delete user -n test --rm-nkey --rm-creds
+  nsc delete user -n myuser --rm-nkey --rm-creds
   exit 0
 fi
 
@@ -12,18 +12,23 @@ fi
 nsc load --profile "nsc://synadia?secret=${NGS_KEY}"
 
 # Create a new user in the current account
-result=$(nsc add user test --allow-sub "_INBOX.>" --allow-pub "ngs.echo")
+echo nsc add user myuser --allow-sub "_INBOX.>" --allow-pub "ngs.echo" --allow-pubsub "$PUBSUB"
+result=$(nsc add user myuser --allow-sub "_INBOX.>" --allow-pub "ngs.echo" --allow-pubsub "$PUBSUB" )
 if [ $? -ne 0 ]; then
   echo $result
   exit 1
 fi
 
+nsc env
+nsc list operators
+nsc list accounts
+nsc list users
+
 # Get path towards credentials file
 OPERATOR=$(nsc env 2>&1 | grep "Current Operator" | awk -F '|' '{print $4}' | xargs)
 ACCOUNT=$(nsc env 2>&1 | grep "Current Account" | awk -F '|' '{print $4}' | xargs)
-CREDS=~/.local/share/nats/nsc/keys/creds/$OPERATOR/$ACCOUNT/test.creds
-# CREDS=~/.nkeys/creds/$OPERATOR/$ACCOUNT/test.creds
-
+CREDS=~/.local/share/nats/nsc/keys/creds/$OPERATOR/$ACCOUNT/myuser.creds
+# CREDS=~/.nkeys/creds/$OPERATOR/$ACCOUNT/myuser.creds
 
 # Create Acornfile containing service and secret
 # cat > /tmp/output<<EOF
